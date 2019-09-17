f83d10d6056e17df15d0d6a1fa99663c6efac8a5
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-17 15:01:33 -0300

    Styling WeatherCond

--- src/containers/WeatherCond.js
+++ src/containers/WeatherCond.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const WeatherCond = () => {
-  const [outWeather, setOutWeather] = useState('');
   const [outTemp, setOutTemp] = useState('');
 
   useEffect(() => {
@@ -11,8 +10,6 @@ const WeatherCond = () => {
         const url = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=980807a99ae3cd8b1117e23106226eb7`;
         const { data } = await axios.get(url);
         const tempCelcius = Math.round(data.main.temp - 273.15);
-
-        setOutWeather(data.weather[0].main);
         setOutTemp(`${tempCelcius}˚C`);
       } catch (error) {
         console.log(error)
@@ -22,12 +19,9 @@ const WeatherCond = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        {outWeather}
-      </div>
-      <div >
-        {outTemp}
+    <div className="app__weather--container">
+      <div className="app__weather--degrees">
+        Outside Temp: {outTemp}
       </div>
     </div>
   );
