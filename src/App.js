af9a4bef6135819cb87f2a400a6e8295ef0353ee
Henrique Fadoni <henrique.fadoni@blindside.pro>
2019-09-17 11:44:57 -0300

    Installing Axios and Creating WeatherCond component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import OnlineUsers from './containers/OnlineUsers'
+import OnlineUsers from './containers/OnlineUsers';
+import WeatherCond from './containers/WeatherCond';
 
 function App() {
   return (
     <div className="App">
       <OnlineUsers />
+      <WeatherCond />
     </div>
   );
 }
--- src/containers/WeatherCond.js
+++ src/containers/WeatherCond.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const WeatherCond = () => {
+  const [outWeather, setOutWeather] = useState('');
+  const [outTemp, setOutTemp] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=980807a99ae3cd8b1117e23106226eb7`;
+        const { data } = await axios.get(url);
+        const tempCelcius = Math.round(data.main.temp - 273.15);
+
+        setOutWeather(data.weather[0].main);
+        setOutTemp(`${tempCelcius}˚C`);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
-      
+      <div>
+        {outWeather}
+      </div>
+      <div >
+        {outTemp}
+      </div>
     </div>
   );
 }
